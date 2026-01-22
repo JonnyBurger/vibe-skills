5d716b0750e73d09213712a55700be4b9ba2c608
Jonny Burger <jonathanburger11@gmail.com>
2026-01-22 12:57:11 +0100

    "use remotion best practices. create a new composition with the following SVG in 3D. add a metallic material to it"

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import { Composition } from "remotion";
 import { MyComposition } from "./Composition";
 import { RemotionTweet } from "./RemotionTweet";
+import { Remotion3DLogo } from "./Remotion3DLogo";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -21,6 +22,14 @@ export const RemotionRoot: React.FC = () => {
         fps={30}
         width={1338}
         height={1914}
+      />
+      <Composition
+        id="Remotion3DLogo"
+        component={Remotion3DLogo}
+        durationInFrames={300}
+        fps={30}
+        width={1920}
+        height={1080}
       />
     </>
   );
